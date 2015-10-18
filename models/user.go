f812ef343ac7f01d06be3ca096d12a1444d4d38c
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-10-18 23:20:43 +0900

    test

--- models/user.go
+++ models/user.go
@@ -4,9 +4,9 @@ import "strconv"
 
 // User defines an user
 type User struct {
-	ID   int
-	Name string
-	Age  int
+	IDD  int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 // NewUser creates user instance
--- server.go
+++ server.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
 	"github.com/codegangsta/negroni"
-	"github.com/shopetan/fastGo/models" // 自分のパッケージ
+	"github.com/shopetan/fastGo/models"
+	"github.com/unrolled/render"
 	"net/http"
 )
 
 func main() {
+	ren := render.New()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		user := models.NewUser("Naoyoshi Aikawa", 29)
-		fmt.Fprintln(w, user)
+		user := models.NewUser("TANAKA tarou", 10)
+		marshalUser, _ := json.Marshal(user)
+		ren.JSON(w, http.StatusOK, user)
 	})
 	n := negroni.Classic()
 	n.UseHandler(mux)
